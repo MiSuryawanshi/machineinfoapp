021b4c92eda0c5c24bd7a541075f6fef22b6edcc
Mangesh Suryawanshi <mangeshsu@cybage.com>
2019-05-09 19:01:39 +0530

    Machine Info POC started.

---
+++ src/main/java/com/cybage/machineinfoapp/MachineinfoappApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.machineinfoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineinfoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineinfoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybage/machineinfoapp/MachineinfoappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cybage.machineinfoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MachineinfoappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
